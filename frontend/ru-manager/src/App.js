dfce2c5c14cfa228f840b80eba5b4e009dfa7df2
JoaoMMenezes <joaopm.m0@gmail.com>
2023-04-18 19:07:30 -0300

    Removed unnused files and adding BrowserRouter

--- frontend/ru-manager/src/App.js
+++ frontend/ru-manager/src/App.js
@@ -1,28 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Landing from './Pages/landing'
+import Home from './Pages/home'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <code>First template do RU-manager project.</code>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return(
+  <Router>
+    <Routes>
+      <Route exact path='/'     element={<Landing/>}/>
+      <Route exact path='/home' element={<Home/>}/>
+    </Routes>
+  </Router>
+  )
 }
 
 export default App;
