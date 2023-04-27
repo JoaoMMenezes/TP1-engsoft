cc83e227e03ad94ae68f4debdb65e4e2c77ad0de
viniguim4 <viniguima14@gmail.com>
2023-04-27 02:59:10 +0000

    [E.g] POST Example.

--- Backend/ru-server/api/__main__.py
+++ Backend/ru-server/api/__main__.py
@@ -1,4 +1,4 @@
-from flask import jsonify
+from flask import jsonify, request, Response
 from api import create_app
 
 app = create_app()
@@ -6,6 +6,13 @@ app = create_app()
 @app.route("/", methods = ["GET"])
 def landing():
     return jsonify("Olá!")
+
+@app.route("/login", methods = ["POST"])
+def login():
+    data = request.data
+    print(data)
+    return  Response(status=204)
+    #return jsonify(data)
 
 if __name__ == "__main__":
     app.run(host = "127.0.0.1")
