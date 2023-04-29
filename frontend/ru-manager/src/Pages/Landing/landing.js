9ae3796368374636db15d3d954ff86263e674585
JoaoMMenezes <joaopm.m0@gmail.com>
2023-04-29 18:09:42 -0300

    [ADD] Login styled

--- frontend/ru-manager/src/Pages/Landing/landing.js
+++ frontend/ru-manager/src/Pages/Landing/landing.js
@@ -2,40 +2,83 @@ import React, { useState } from "react";
 import './landing.css'
 
 function Landing() {
-  const [id, setId] = useState('');
-  const [password, setPassword] = useState('');
+  // React States
+  const [errorMessages, setErrorMessages] = useState({});
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log('ID:', id);
-    console.log('Password:', password);
+  // User Info - TODO: colletc from db
+  const database = [
+    {
+      username: "user1",
+      password: "pass1"
+    },
+    {
+      username: "user2",
+      password: "pass2"
+    }
+  ];
+
+  const errors = {
+    uname: "invalid username",
+    pass: "invalid password"
   };
 
-  return (
-    <div>
+  const handleSubmit = (event) => {
+    //Prevent page reload
+    event.preventDefault();
+
+    var { uname, pass } = document.forms[0];
+
+    // Find user login info
+    const userData = database.find((user) => user.username === uname.value);
+
+    // Compare user info
+    if (userData) {
+      if (userData.password !== pass.value) {
+        // Invalid password
+        setErrorMessages({ name: "pass", message: errors.pass });
+      } else {
+        setIsSubmitted(true);
+      }
+    } else {
+      // Username not found
+      setErrorMessages({ name: "uname", message: errors.uname });
+    }
+  };
+
+  // Generate JSX code for error message
+  const renderErrorMessage = (name) =>
+    name === errorMessages.name && (
+      <div className="error">{errorMessages.message}</div>
+    );
+
+  // JSX code for login form
+  const renderForm = (
+    <div className="form">
       <form onSubmit={handleSubmit}>
-        <h1>Login</h1>
-        <label>
-          
-          <input
-            
-            required
-            type="number"
-            value={id}
-            onChange={(e) => setId(e.target.value)}
-          />
-        </label>
-        <label>
-          Password:
-          <input
-            required
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </label>
-        <button type="submit">Login</button>
+        <div className="input-container">
+          <label>Usuário </label>
+          <input type="text" name="uname" required />
+          {renderErrorMessage("uname")}
+        </div>
+        <div className="input-container">
+          <label>Senha </label>
+          <input type="password" name="pass" required />
+          {renderErrorMessage("pass")}
+        </div>
+        <div className="button-container">
+          <input type="submit" />
+        </div>
       </form>
+    </div>
+  );
+
+  return (
+    <div className="app">
+      <div className="login-form">
+        <div className="title">Entrar</div>
+        {isSubmitted ? <div>User is successfully logged in</div> : renderForm}
+      </div>
     </div>
   );
 }
