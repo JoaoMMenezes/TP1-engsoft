b5e72962140f488404f2fa9532db22eb70f9ff71
leonelsmp <leonelpalhares@gmail.com>
2023-04-29 19:46:54 -0300

    [SOLVE] merging problems

--- Backend/app.py
+++ Backend/app.py
@@ -3,7 +3,12 @@ from flask import Flask, jsonify, request
 from utils.dbacess import ServerAcess
 #
 from src.user0.comer import comer, parseComer
+
 from src.Login.logar import parseLogar, identificarUser, buscarUser, conferirSenha
+
+from src.user0.signin import signIn, parseSignIn
+from src.user0.depositoken import deposit, parseDeposit
+
 
 
 app = Flask(__name__)
@@ -11,7 +16,7 @@ app = Flask(__name__)
 ACESSO = ServerAcess("LAPTOP-4BELV735", "credito_ru")
 
 @app.route("/", methods = ["GET"])
-def Home():
+def HelloPage():
     return jsonify({"teste": "oi"})
 
 @app.route("/login", methods = ["POST"])
@@ -45,6 +50,27 @@ def haveLunch():
     except ValueError:
         print("não conseguiu comer")
     return jsonify({"teste": "executado"})
+
+@app.route("/user0/signin", methods = ["POST"])
+def rotaSignIn():
+    try:
+        raw_dados  = request.data.decode('utf-8')
+        matricula, senha, nome, valorFicha, email = parseSignIn(raw_dados)
+        signIn(ACESSO, matricula, senha, nome, valorFicha, email)
+    except ValueError:
+        print("Nao foi possivel cadastrar o usuario")
+    return jsonify({"teste": "Usuário Cadastrado!"})
+
+@app.route("/user0/deposittoken", methods = ["POST"])
+def depositToken():
+    try:
+        raw_dados  = request.data.decode('utf-8')
+        matricula, amount = parseDeposit(raw_dados)
+        deposit(matricula, ACESSO, amount)
+    except ValueError:
+        print("nao foi possivel depositar")
+    return jsonify({"Saldo depositado!"})
+
 
 
 
