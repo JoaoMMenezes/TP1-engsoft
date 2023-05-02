1c69ba328c531f47a1dfef33378164ff2b18006a
leonelsmp <leonelpalhares@gmail.com>
2023-05-01 21:03:14 -0300

    mudancas finais no backend

--- frontend/ru-manager/src/Pages/Home_u2/home_u2.js
+++ frontend/ru-manager/src/Pages/Home_u2/home_u2.js
@@ -1,31 +1,26 @@
-
 import React, { useState } from "react";
 import api from "../../Services/api";
+import './home_u2.css'
 
-// Funcionalidades:
-//      - Apresentar uma tabela dos usuários que usaram os restaurantes em X tempos
-//      - Apresentar os dados de compra de crédito registrados pelos usuários nível 1 (?)
-
-function HomeU2()  {
+function HomeU2() {
   // React States
   const [errorMessages, setErrorMessages] = useState({});
   const [isSubmitted, setIsSubmitted] = useState(false);
 
   const errors = {
-    uname: "invalid username",
-    pass: "invalid password"
+    data1: "invalid data1",
+    data2: "invalid data2"
   };
 
   const handleSubmit = (event) => {
     //Prevent page reload
     event.preventDefault();  
     var { data1, data2 } = document.forms[0];
-    console.log(data1, data2 )
-
+    console.log(data1,data2)
     // Find user login info
     api.post("/user2/getfinance", {
-        DataInicio: data1.value,
-        DataFinal: data2.value
+      DataInicio: data1.value,
+      DataFinal: data2.value
     })
     .then(
       response => console.log(response)
@@ -34,27 +29,27 @@ function HomeU2()  {
       error => console.log(error)
     )
     console.log("--------------")
-
   };
 
   // Generate JSX code for error message
   const renderErrorMessage = (name) =>
-  name === errorMessages.name && (
-    <div className="error">{errorMessages.message}</div>
-  );
+    name === errorMessages.name && (
+      <div className="error">{errorMessages.message}</div>
+    );
+
   // JSX code for login form
   const renderForm = (
     <div className="form">
       <form onSubmit={handleSubmit}>
         <div className="input-container">
-          <label>Usuário </label>
-          <input type="text" name="uname" required />
-          {renderErrorMessage("uname")}
+          <label>UsuÃ¡rio </label>
+          <input type="text" name="data1" required />
+          {renderErrorMessage("data1")}
         </div>
         <div className="input-container">
           <label>Senha </label>
-          <input type="password" pattern="[0-9]*" inputmode="numeric" name="pass" required />
-          {renderErrorMessage("pass")}
+          <input type="data2" name="data2" required />
+          {renderErrorMessage("data2")}
         </div>
         <div className="button-container">
           <input type="submit" />
@@ -74,5 +69,3 @@ function HomeU2()  {
 }
 
 export default HomeU2
-
-
